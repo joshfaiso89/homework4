19e43afc49d25703edc810c580371265ed4934ff
Josh Faison <Joshfaiso@yahoo.com>
2020-01-09 00:54:57 -0500

    timer

--- assets/JSquiz.js
+++ assets/JSquiz.js
@@ -1,15 +1,15 @@
 // select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+const start = document.getElementById("start")
+const quiz = document.getElementById("quiz")
+const question = document.getElementById("question")
+const qImg = document.getElementById("qImg")
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
+const counter = document.getElementById("counter")
+const timeGauge = document.getElementById("timeGauge")
+const progress = document.getElementById("progress")
+const scoreDiv = document.getElementById("scoreContainer")
 
 // create our questions
 let questions = [
@@ -37,54 +37,54 @@ let questions = [
     choiceC: "JavaScript",
     correct: "C"
   }
-];
+]
 
 // create some variables
 
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-let score = 0;
-timer = "timer";
-var scores = [];
+const lastQuestion = questions.length - 1
+let runningQuestion = 0
+let count = 0
+let questionTime = questions.length * 15 // 10s
+const gaugeWidth = 150 // 150px
+const gaugeUnit = gaugeWidth / questionTime
+let TIMER
+let score = 0
+timer = "timer"
+var scores = []
 
-if(localStorage.getItem('score')){
-  var oldArr = scores;
-  scores = oldArr.concat(JSON.parse(localStorage.getItem('score')));
-  console.log(scores);
+if (localStorage.getItem("score")) {
+  var oldArr = scores
+  scores = oldArr.concat(JSON.parse(localStorage.getItem("score")))
+  console.log(scores)
 }
 
 // render a question
 function renderQuestion() {
-  let q = questions[runningQuestion];
+  let q = questions[runningQuestion]
 
-  question.innerHTML = "<p>" + q.question + "</p>";
-  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
+  question.innerHTML = "<p>" + q.question + "</p>"
+  qImg.innerHTML = "<img src=" + q.imgSrc + ">"
+  choiceA.innerHTML = q.choiceA
+  choiceB.innerHTML = q.choiceB
+  choiceC.innerHTML = q.choiceC
 }
 
-start.addEventListener("click", startQuiz);
+start.addEventListener("click", startQuiz)
 
 // start quiz
 function startQuiz() {
-  start.style.display = "none";
-  renderQuestion();
-  quiz.style.display = "block";
-  renderProgress();
-  renderCounter();
-  TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+  start.style.display = "none"
+  renderQuestion()
+  quiz.style.display = "block"
+  renderProgress()
+  renderCounter()
+  TIMER = setInterval(renderCounter, 1000) // 1000ms = 1s
 }
 
 // render progress
 function renderProgress() {
   for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>"
   }
 }
 
@@ -92,20 +92,20 @@ function renderProgress() {
 
 function renderCounter() {
   if (count <= questionTime) {
-    counter.innerHTML = count;
-    timeGauge.style.width = count * gaugeUnit + "px";
-    count++;
+    counter.innerHTML = count
+    timeGauge.style.width = count * gaugeUnit + "px"
+    questionTime--
   } else {
-    count = 0;
+    count = 0
     // change progress color to red
-    answerIsWrong();
+    answerIsWrong()
     if (runningQuestion < lastQuestion) {
-      runningQuestion++;
-      renderQuestion();
+      runningQuestion++
+      renderQuestion()
     } else {
       // end the quiz and show the score
-      clearInterval(TIMER);
-      scoreRender();
+      clearInterval(TIMER)
+      scoreRender()
     }
   }
 }
@@ -115,54 +115,48 @@ function renderCounter() {
 function checkAnswer(answer) {
   if (answer == questions[runningQuestion].correct) {
     // answer is correct
-    score++;
+    score++
     // change progress color to green
-    answerIsCorrect();
+    answerIsCorrect()
   } else {
     // answer is wrong
     // change progress color to red
-    answerIsWrong();
+    answerIsWrong()
   }
-  count = 0;
+  count = 0
   if (runningQuestion < lastQuestion) {
-    runningQuestion++;
-    renderQuestion();
+    runningQuestion++
+    renderQuestion()
   } else {
     // end the quiz and show the score
-    clearInterval(TIMER);
-    scoreRender();
+    clearInterval(TIMER)
+    scoreRender()
   }
 }
 
 // answer is correct
 function answerIsCorrect() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+  document.getElementById(runningQuestion).style.backgroundColor = "#0f0"
 }
 
 // answer is Wrong
 function answerIsWrong() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+  document.getElementById(runningQuestion).style.backgroundColor = "#f00"
 }
 
 // score render
 function scoreRender() {
-  document.getElementById("quiz").style.display = "none";
-  scoreDiv.style.display = "block";
+  document.getElementById("quiz").style.display = "none"
+  scoreDiv.style.display = "block"
 
   // calculate the amount of question percent answered by the user
-  const scorePerCent = Math.round((100 * score) / questions.length);
-  scorePerCent.textContent = "Your Score is" + scoreRender + "%!";
+  const scorePerCent = Math.round((100 * score) / questions.length)
+  scorePerCent.textContent = "Your Score is" + scoreRender + "%!"
 
-  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>"
 
-  console.log(scorePerCent);
-  scores.push(scorePerCent);
-  console.log(scores);
-  localStorage.setItem('score', JSON.stringify(scores));
-
-
+  console.log(scorePerCent)
+  scores.push(scorePerCent)
+  console.log(scores)
+  localStorage.setItem("score", JSON.stringify(scores))
 }
-
-
-
-
